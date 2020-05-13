62ea99b17ffbacae9369b32ba4c4198eb70e1e18
andatu <andre.aturai@outlook.com>
2020-05-13 21:38:18 +0200

    sem4 v.3

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -94,4 +94,5 @@ public class Controller {
         observerList.add(obs);
     }
 
+
 }
--- src/model/Observer.java
+++ src/model/Observer.java
@@ -1,7 +1,5 @@
 package model;
 
-import model.CashRegister;
-
 public interface Observer {
     void addPayment(double payment);
 }
